19cbbf731fd71fcd5a0daf9ec2598efd6095ade5
Jaakko Ojalehto <jaakko@ojalehto.fi>
2018-09-03 00:08:43 +0300

    Add ship deckplan patterns

--- src/generators/ship-deckplan.js
+++ src/generators/ship-deckplan.js
@@ -7,10 +7,17 @@ const svg = d3.select('svg')
 let labels = []
 const corridorHeight = 5 * 10
 
+const patterns = [
+  'Rectangle',
+  'Triangle',
+  'Random',
+]
+
 export function generateDeckplan(hullType, fittings) {  
   svg.selectAll("*").remove()
   getLabels(fittings)
-  const sections = createSections(hullType)
+  const pattern = patterns[random(0, patterns.length - 1)]
+  const sections = createSections(pattern, hullType)
   const rooms = createRooms(sections)
   flatten(rooms).forEach(room => {
     svg.append('rect')
@@ -53,19 +60,15 @@ function createBridge() {
   return { width, height, x, y, label: 'Bridge' }
 }
 
-function createSections(hullType) {
+function createSections(pattern, hullType) {
   let roomsLeft = hullType.maxRooms
   let sections = []
   let x = 10
   let sectionNumber = 1
-  let corridorLength = random(0, roomsLeft / 2)
+  let corridorLength = calculateCorridorLength(pattern, hullType.maxRooms)
   while(roomsLeft > 0) {
     const width = random(10, 20) * 10
-    const maxRoomsOnThisSection = Math.min(3, hullType.maxRooms, sectionNumber)
-    let roomCount = random(1, maxRoomsOnThisSection)
-    if (corridorLength > 0) {
-      roomCount = random(Math.min(2, maxRoomsOnThisSection), Math.min(2, maxRoomsOnThisSection))
-    }
+    const roomCount = calculateSectionRoomCount(pattern, hullType.maxRooms, sectionNumber, roomsLeft)
     roomsLeft -= roomCount
     sections = [...sections, { x, width, roomCount, corridorLength }]
     x += width
@@ -75,52 +78,86 @@ function createSections(hullType) {
   return sections
 }
 
+function calculateCorridorLength(pattern, maxRooms) {
+  switch (pattern) {
+    case 'Rectangle':
+      return random(0, Math.floor(maxRooms / 3))
+    case 'Triangle':
+      return random(0, 2)
+    case 'Random':
+      return random(0,Math.floor(maxRooms / 2))
+  }
+}
+
+function calculateSectionRoomCount(pattern, maxRooms, sectionNumber, roomsLeft) {
+  switch (pattern) {
+    case 'Rectangle':
+      return Math.max(1, Math.floor(Math.sqrt(maxRooms)))
+    case 'Triangle':
+      return sectionNumber
+    case 'Random':
+      return Math.max(1, random(1, Math.floor(maxRooms / 3)))
+  }
+}
+
 function createRooms(sections) {
   const corridor = createCorridor(sections)
   const rooms = sections.map((section) => {
     const { roomCount, width, x, corridorLength } = section
     if (roomCount === 1) {
-      return createCenterRoom(x, width)
+      return createOneRoom(x, width)
+    }
+    if (roomCount % 2 === 1) {
+      return createOddRooms(x, width, roomCount)
     }
     if (corridorLength > 0) {
       return createMainCorridorRooms(x, width, roomCount)
     }
-    if (roomCount === 2) {
-      return createDoubleRooms(x, width)
-    }
-    if (roomCount === 3) {
-      return createTripleRooms(x, width)
+    if (roomCount % 2 === 0) {
+      return createEvenRooms(x, width, roomCount)
     }
   })
   return corridor ? [corridor, ...rooms] : rooms
 }
 
-function createCenterRoom(x, width) {
+function createOneRoom(x, width) {
   const height = random(10, 30) * 10
   const y = (svgHeight - height) / 2
   return [{ width, height, x, y, label: labels.pop() }]
 }
 
-function createDoubleRooms(x, width) {
+function createEvenRooms(x, width, roomCount) {
   const height = random(10, 20) * 10
-  const firstY = svgHeight / 2 - height
-  const secondY = svgHeight / 2
-  return [
-    { width, height, x, y: firstY, label: labels.pop() },
-    { width, height, x, y: secondY, label: labels.pop() }
-  ]
+  let rooms = []
+  for (let i = 1; i * 2 <= roomCount; i ++) {
+    const upperY = svgHeight / 2 - height * i
+    const lowerY = svgHeight / 2 + (height * (i - 1))
+    rooms = [
+      ...rooms,
+      { width, height, x, y: upperY, label: labels.pop() },
+      { width, height, x, y: lowerY, label: labels.pop() },
+    ]
+  }
+  return rooms
 }
 
-function createTripleRooms(x, width) {
+function createOddRooms(x, width, roomCount) {
   const centerHeight = random(10, 20) * 10
   const sideHeight = random(10, 20) * 10
-  const firstY = (svgHeight - centerHeight) / 2
-  const secondY = (svgHeight - centerHeight) / 2 - sideHeight
-  const thirdY = (svgHeight - centerHeight) / 2 + centerHeight
+  const centerY = (svgHeight - centerHeight) / 2
+  let rooms = [{ width, height: centerHeight, x, y: centerY, label: labels.pop() }]
+  for (let i = 1; i * 2 <= roomCount; i ++) {
+    const upperY = (svgHeight - centerHeight) / 2 - (sideHeight * i)
+    const lowerY = (svgHeight - centerHeight) / 2 + centerHeight + (sideHeight * (i - 1))
+    rooms = [
+      ...rooms,
+    { width, height: sideHeight, x, y: upperY, label: labels.pop() },
+    { width, height: sideHeight, x, y: lowerY, label: labels.pop() }
+    ]
+  }
+  return rooms
   return [
-    { width, height: centerHeight, x, y: firstY, label: labels.pop() },
-    { width, height: sideHeight, x, y: secondY, label: labels.pop() },
-    { width, height: sideHeight, x, y: thirdY, label: labels.pop() }
+
   ]
 }
 
@@ -136,7 +173,14 @@ function createMainCorridorRooms(x, width) {
 }
 
 function createCorridor(sections) {
-  const corridorSections = sections.filter(section => section.corridorLength > 0 && section.roomCount > 1)
+  let corridorSections = []
+  let corridorNotEnded = true
+  sections.forEach(section => {
+    corridorNotEnded = section.corridorLength > 0 && section.roomCount % 2 === 0
+    if (corridorNotEnded) {
+      corridorSections = [...corridorSections, section]
+    }
+  })
   if(corridorSections.length === 0) {
     return
   }
